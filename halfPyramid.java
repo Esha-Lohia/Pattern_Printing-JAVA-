2dfdf274db2ca2f9a1c9395fab3d44966dfc1cfc
Esha Lohia <eshalohia5678@gmail.com>
2025-08-11 10:45:55 +0530

    Update halfPyramid.java

--- halfPyramid.java
+++ halfPyramid.java
@@ -1,1 +1,15 @@
-
+package Pattern_Printing-JAVA-;
+import java.util.Scanner;
+public class halfPyramid {
+  public static void main(String[] args) {
+    int n=4;
+    //Outer loop
+    for(int i=1; i<=n; i++){
+      //Inner Loop
+      for(int j=1; j<=i; j++){
+        System.out.print("*");
+      }
+       System.out.println("");
+    }
+  }
+}
