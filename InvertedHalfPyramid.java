438fb2c3457c5848cb7bb913db48449a75ef615a
Esha Lohia <eshalohia5678@gmail.com>
2025-08-11 10:50:14 +0530

    Update InvertedHalfPyramid.java

--- InvertedHalfPyramid.java
+++ InvertedHalfPyramid.java
@@ -1,1 +1,16 @@
+package Pattern_Printing-JAVA-;
+import java.util.Scanner;
+public class InvertedHalfPyramid {
+  public static void main(String[] args) {
+    int n=5;
+    //Outer Loop
+  for(int i=n; i>=1; i--){
+    //Inner Loop
+    for(int j=1; j<=i ; j++){
+      System.out.print("*");
+    }
+    System.out.println("");
+  }
+}
+}
 
