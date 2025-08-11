7d29dfa24a8bdd1a946c387c3c5fbe74845330a1
Esha Lohia <eshalohia5678@gmail.com>
2025-08-11 10:31:11 +0530

    Update solidRectangle.java

--- solidRectangle.java
+++ solidRectangle.java
@@ -1,1 +1,14 @@
+package Pattern_Printing-JAVA-;
+import java.util.Scanner;
 
+public class solidRectangle {
+    public static void main(String[] args) {
+      for(int i=1; i<=4; i++){
+        for(int j=1; j<=5; j++){
+          System.out.print("*");
+    }
+    System.out.println(" ");
+      }      
+    }
+  }  
+  
