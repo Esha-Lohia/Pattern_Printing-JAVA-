811e1b468d29d62d0876d4674213a29d1525ba0a
Esha Lohia <eshalohia5678@gmail.com>
2025-08-11 15:17:42 +0530

    Create 0-1Triangle.java

---
+++ 0-1Triangle.java
@@ -0,0 +1,20 @@
+public class ZeroOneTriangle {
+  public static void main(String[] args) {
+    int n=5;
+    //Outer Loop
+    for(int i=1; i<=n;i++){
+      //Inner Loop
+      for(int j=1; j<=i; j++){
+        int sum=i+j;
+        if(sum%2==0){
+          System.out.print("1");
+        }
+        else{
+          System.out.print("0");
+        }
+      }
+      System.out.println("");
+    }
+  }
+  
+}
