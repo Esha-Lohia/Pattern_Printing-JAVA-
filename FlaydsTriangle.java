7ba447e260f50b1d0ade90ab25d78683c85fcdfa
Esha Lohia <eshalohia5678@gmail.com>
2025-08-11 15:13:00 +0530

    Create FlaydsTriangle.java

---
+++ FlaydsTriangle.java
@@ -0,0 +1,16 @@
+public class FlaydsTriangle {
+  public static void main(String[] args) {
+    int n=5;
+    int num=1;
+
+    //Outer Loop
+    for(int i=1; i<=n;i++){
+      //Inner Loop
+      for(int j=1; j<=i; j++){
+        System.out.print(num+" ");
+        num++;
+      }
+      System.out.println("");
+    }
+}
+}
