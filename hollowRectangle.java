bb85384d3be9644559c64fd02639315ab14a5382
Esha Lohia <eshalohia5678@gmail.com>
2025-08-11 10:40:17 +0530

    Update hollowRectangle.java

--- hollowRectangle.java
+++ hollowRectangle.java
@@ -1,1 +1,18 @@
+package Pattern_Printing-JAVA-;
+import java.util.Scanner;
 
+public class hollowRectangle {
+  public static void main(String[] args) {
+    for(int i=1;i<=5;i++){
+      for(int j=1; j<=5; j++){
+        if(i==1|| j==1||j==5||i==5){
+          System.out.print("*");
+        }
+        else{
+          System.out.print(" ");
+        }
+    }
+  System.out.println("");  
+}
+}
+}
